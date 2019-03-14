504de986e5f5150caca33e8e836a89176f271ee5
Tanner-Mindrum <tannermindrum@gmail.com>
2019-03-13 20:53:50 -0700

    Programming assignment 3

---
+++ Discrete Structures with Computing Applications II (CECS 229)/Number Theory & Cryptography/Programming Assignment 3-Tanner Mindrum.py
@@ -0,0 +1,53 @@
+#Tanner Mindrum and Hunter Davis
+from math import gcd
+
+# Chinese Remainder Theorem (See Theorem 2 of Section 4.4):
+# Write a function that will take in two list inputs: aList and mList.
+# aList will contain the list of a values and mList will contain the list of m values.
+# Your function should then provide the final x value after it has been modded by m.
+# The x value should be returned as an integer.
+def relativelyPrime(L):
+    for i in range(len(L)):
+        for j in range(i+1,len(L)):
+            if gcd(L[i],L[j]) != 1:
+                return "Not Pairwise Relatively Prime"
+    return "Pairwise Relatively Prime"
+
+def ChineseRemainderTheorem(a_list, m_list):
+    m = 1
+    M = 1
+    y_count = 1
+    M_list = []
+    y_list = []
+    x = 0
+
+    if len(a_list) != len(m_list):
+        return "Lists are not of the same length!"
+
+    for i in range(len(m_list)):
+        if (m_list[i] <= 1):
+            return "Cannot proceed, mList input not pairwise relatively prime"
+
+    if relativelyPrime(m_list) != "Pairwise Relatively Prime":
+        return "Cannot proceed, mList input not pairwise relatively prime"
+
+    else:
+        for i in range(len(m_list)):
+            m *= m_list[i]
+
+        for i in range(len(m_list)):
+            M_list.append(m // m_list[i])
+
+        for i in range(len(m_list)):
+            while True:
+                if (M_list[i] * y_count % m_list[i] != 1):
+                    y_count += 1
+                else:
+                    break
+            y_list.append(y_count)
+
+        for i in range(len(m_list)):
+            x += (a_list[i] * M_list[i] * y_list[i])
+        x %= m
+
+    return x
